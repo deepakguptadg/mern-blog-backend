33881d457e10321c575a8c0ea7c4de9a61571c6d
Deepak Gupta <deepakgupta30400@gmail.com>
2022-09-03 18:13:47 +0530

    Backend

---
+++ app.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+import config from "./database/config.js";
+const app = express()
+import cors from 'cors'
+import web from './routes/web.js'
+import contact from './routes/ContactRoutes.js'
+const DATABASE_URL = "mongodb://localhost:27017"
+config(DATABASE_URL)
+const port = 4000
+app.use(express.json())
+app.use(cors())
+app.use('/blog', web)
+app.use('/contact', contact)
+
+app.use(express.static('public/upload/img'))
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
